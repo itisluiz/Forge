50fbdf69eea386077da8cf1d885e10508ff9879b
Leonardo Alves <90728273+alvessleo@users.noreply.github.com>
2024-04-26 11:10:06 -0300

    Setup inicial

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,14 +3,13 @@ import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { AgileProcessComponent } from "./core/components/agile-process/agile-process.component";
 import { NavbarComponent } from "./core/components/navbar/navbar.component";
-import { ChatBotComponent } from "./core/components/chat-bot/chat-bot.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
-	imports: [CommonModule, RouterOutlet, AgileProcessComponent, NavbarComponent, ChatBotComponent],
+	imports: [CommonModule, RouterOutlet, AgileProcessComponent, NavbarComponent],
 })
 export class AppComponent {
 	title = "forge-frontend";
