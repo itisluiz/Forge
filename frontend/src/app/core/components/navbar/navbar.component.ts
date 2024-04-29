44816bfeb528267427907924ac85783a38ba0eb7
Leonardo Alves <90728273+alvessleo@users.noreply.github.com>
2024-04-29 13:58:27 -0300

    Kanban page + components

--- frontend/src/app/core/components/navbar/navbar.component.ts
+++ frontend/src/app/core/components/navbar/navbar.component.ts
@@ -2,11 +2,18 @@ import { Component } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
 	selector: "app-navbar",
 	standalone: true,
-	imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+	imports: [
+		MatToolbarModule, 
+		MatButtonModule, 
+		MatIconModule,
+		MatSidenavModule
+	],
 	templateUrl: "./navbar.component.html",
 	styleUrl: "./navbar.component.scss",
 })
---
+++ frontend/src/app/core/components/pages/kanban-page/kanban-page.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from "@angular/core";
+import { NavbarComponent } from "../../navbar/navbar.component";
+import { MatIcon } from "@angular/material/icon";
+import { MatExpansionModule } from '@angular/material/expansion';
+import {
+	CdkDragDrop,
+	CdkDrag,
+	CdkDropList,
+	CdkDropListGroup,
+	moveItemInArray,
+	transferArrayItem,
+	DragDropModule,
+	CdkDragPlaceholder,
+  } from '@angular/cdk/drag-drop';
+
+@Component({
+	selector: "app-kanban-page",
+	standalone: true,
+	imports: [
+		NavbarComponent, 
+		MatIcon, 
+		MatExpansionModule, 
+		DragDropModule,
+		CdkDropListGroup, 
+		CdkDropList, 
+		CdkDrag,
+		CdkDragPlaceholder
+	],
+	templateUrl: "./kanban-page.component.html",
+	styleUrl: "./kanban-page.component.scss",
+})
+export class KanbanPageComponent implements OnInit {
+
+	ngOnInit(): void {}
+
+	toDo: any= [
+		{
+		  name: 'Get to work',
+		  description: 'Description for Get to work',
+		  currentBehavior: 'Current behavior for Get to work',
+		  expectedBehavior: 'Expected behavior for Get to work',
+		  photo: '../../../assets/photo.jpeg',
+		  type: 'feature'
+		},
+		{
+		  name: 'Pick up groceries',
+		  description: 'Description for Pick up groceries',
+		  currentBehavior: 'Current behavior for Pick up groceries',
+		  expectedBehavior: 'Expected behavior for Pick up groceries',
+		  photo: '../../../assets/photo.jpeg',
+		  type: 'feature'
+		},
+		{
+		  name: 'Go home',
+		  description: 'Description for Go home',
+		  currentBehavior: 'Current behavior for Go home',
+		  expectedBehavior: 'Expected behavior for Go home',
+		  photo: '../../../assets/photo.jpeg',
+		  type: 'feature'
+		},
+		{
+		  name: 'Fall asleep',
+		  description: 'Description for Fall asleep',
+		  currentBehavior: 'Current behavior for Fall asleep',
+		  expectedBehavior: 'Expected behavior for Fall asleep',
+		  photo: '../../../assets/photo.jpeg',
+		  type: 'feature'
+		}
+	];
+
+	inProgress: any = [
+		
+	];
+
+	availableReview: any = [
+
+	];
+
+	reviewing: any = [
+
+	];
+  
+  	done: any = [
+
+	];
+
+	
+
+  	drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
