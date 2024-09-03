cec09b52a927dc1ac79fcb2885a9cd8a40d1a2d0
Leonardo Alves <alvesleo2002@gmail.com>
2024-09-02 21:14:05 -0300

    feat: search bar + navegation

--- frontend/src/app/core/components/navbar/navbar.component.ts
+++ frontend/src/app/core/components/navbar/navbar.component.ts
@@ -3,12 +3,91 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { map, Observable, startWith } from "rxjs";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { AsyncPipe, CommonModule } from "@angular/common";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProjectResponse } from "forge-shared/dto/response/projectresponse.dto";
+import { ProjectMemberComposite } from "forge-shared/dto/composite/projectmembercomposite.dto";
+import { ProjectApiService } from "../../services/project-api.service";
+import { UserApiService } from "../../services/user-api.service";
+
+export interface SearchBar {
+	name: string;
+	route: string;
+}
 
 @Component({
 	selector: "app-navbar",
 	standalone: true,
-	imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
+	imports: [
+		MatToolbarModule,
+		MatButtonModule,
+		MatIconModule,
+		MatSidenavModule,
+		MatAutocompleteModule,
+		MatInputModule,
+		MatFormFieldModule,
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		AsyncPipe,
+	],
 	templateUrl: "./navbar.component.html",
 	styleUrl: "./navbar.component.scss",
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+	projectEid: string = this.route.snapshot.paramMap.get("projectEid")!;
+	userPhoto!: string;
+
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute,
+		private userApiService: UserApiService,
+	) {}
+
+	myControl = new FormControl<string | SearchBar>("");
+	options: SearchBar[] = [
+		{ name: "Project Epics", route: `/${this.projectEid}/epics` },
+		{ name: "Project Backlog", route: `/${this.projectEid}/backlog` },
+		{ name: "Sprint Board", route: `/${this.projectEid}/kanban` },
+	];
+
+	filteredOptions!: Observable<SearchBar[]>;
+
+	ngOnInit() {
+		this.getUser();
+
+		this.filteredOptions = this.myControl.valueChanges.pipe(
+			startWith(""),
+			map((value) => {
+				const name = typeof value === "string" ? value : value?.name;
+				return name ? this._filter(name as string) : this.options.slice();
+			}),
+		);
+	}
+
+	getUser() {
+		this.userApiService.self().subscribe((user) => {
+			this.userPhoto = user.gravatar;
+		});
+	}
+
+	displayFn(user: SearchBar): string {
+		return user && user.name ? user.name : "";
+	}
+
+	navigateTo(route: string) {
+		console.log(route);
+		this.router.navigate([route]);
+	}
+
+	private _filter(name: string): SearchBar[] {
+		const filterValue = name.toLowerCase();
+
+		return this.options.filter((option) => option.name.toLowerCase().includes(filterValue));
+	}
+}
