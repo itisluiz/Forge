8b6c02f4fa32924adc050fc9e5d74fa6091b3ba5
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2024-09-26 23:12:05 -0300

    User no longer wants to build muscle; Fixes (hopefully) timezone nonsense

--- backend/src/mappers/composite/burndownentriescomposite.mapper.ts
+++ backend/src/mappers/composite/burndownentriescomposite.mapper.ts
@@ -4,11 +4,10 @@ export function mapBurndownEntriesComposite(sprint: any): BurndownEntryComposite
 	const now = new Date();
 	const result: BurndownEntryComposite[] = [];
 
-	for (
-		let date = new Date(sprint.dataValues.startsAt);
-		date <= sprint.dataValues.endsAt && date <= now;
-		date.setDate(date.getDate() + 1)
-	) {
+	const firstDate = new Date(sprint.dataValues.startsAt);
+	firstDate.setDate(firstDate.getDate() + 1);
+
+	for (let date = firstDate; date <= sprint.dataValues.endsAt && date <= now; date.setDate(date.getDate() + 1)) {
 		const entry: BurndownEntryComposite = {
 			date: date.toISOString(),
 			effort: sprint.calculateBurndownForDate(date),
