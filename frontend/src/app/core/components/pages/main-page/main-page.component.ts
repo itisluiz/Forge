e678a05d5097124f2f188152af413c0202967833
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2024-09-29 20:23:41 -0300

    Lead time and sprint velocity metric working.

--- frontend/src/app/core/components/pages/main-page/main-page.component.ts
+++ frontend/src/app/core/components/pages/main-page/main-page.component.ts
@@ -10,24 +10,31 @@ import { SprintResponse } from "forge-shared/dto/response/sprintresponse.dto";
 import { TaskSelfComposite } from "forge-shared/dto/composite/taskselfcomposite.dto";
 import { TaskStatus } from "forge-shared/enum/taskstatus.enum";
 import { BurndownChartComponent } from "../../burndown-chart/burndown-chart.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
 	selector: "app-main-page",
 	standalone: true,
-	imports: [AgileProcessComponent, NavbarComponent, MatIconModule, RouterModule, BurndownChartComponent],
+	imports: [AgileProcessComponent, NavbarComponent, MatIconModule, RouterModule, BurndownChartComponent, CommonModule],
 	templateUrl: "./main-page.component.html",
 	styleUrl: "./main-page.component.scss",
 })
 export class MainPageComponent {
 	projectEid: string = this.route.snapshot.paramMap.get("projectEid")!;
-	currentAndFutureSprints: SprintSelfComposite[] = [];
+	allSprints: SprintSelfComposite[] = [];
 	currentSprint: SprintSelfComposite = {} as SprintSelfComposite;
+	pastAndCurrentSprints: SprintSelfComposite[] = [];
 	pastSprints: SprintSelfComposite[] = [];
 	selectedSprint: SprintSelfComposite = {} as SprintSelfComposite;
-	detailedSprint: SprintResponse = {} as SprintResponse;
 	tasksFromSelectedSprint: TaskSelfComposite[] = [];
-	leadTimeDaysAverage: number = 0;
-	sprintVelocity: number = 0;
+
+	sprintLeadTime: string = "-";
+	averageSprintsLeadTime: string = "-";
+	sprintVelocity: string = "-";
+	averageSprintsVelocity: string = "-";
+
+	leadTimeMetricImageUrl: string = "";
+	velocityMetricImageUrl: string = "";
 
 	constructor(
 		private route: ActivatedRoute,
@@ -41,36 +48,43 @@ export class MainPageComponent {
 	loadSprintData() {
 		this.sprintApiService.self(this.projectEid).subscribe({
 			next: (sprintSelfResponse) => {
-				this.currentAndFutureSprints = sprintSelfResponse.sprints.filter(
-					(sprint) =>
-						sprint.periodStatus === SprintPeriodStatus.ONGOING || sprint.periodStatus === SprintPeriodStatus.FUTURE,
+				this.allSprints = sprintSelfResponse.sprints;
+				this.pastAndCurrentSprints = sprintSelfResponse.sprints.filter(
+					(sprint) => sprint.periodStatus === SprintPeriodStatus.PAST || sprint.periodStatus === SprintPeriodStatus.ONGOING,
 				);
 				this.currentSprint =
-					this.currentAndFutureSprints.find((sprint) => sprint.periodStatus === SprintPeriodStatus.ONGOING) ||
+					this.pastAndCurrentSprints.find((sprint) => sprint.periodStatus === SprintPeriodStatus.ONGOING) ||
 					({} as SprintSelfComposite);
 				this.selectedSprint = this.currentSprint;
 				this.pastSprints = sprintSelfResponse.sprints.filter((sprint) => sprint.periodStatus === SprintPeriodStatus.PAST);
-				this.loadLeadTimeMetrics();
+				this.loadMetrics();
 			},
 		});
 	}
 
 	setSprint(event: any) {
 		const selectedValue = event.target.value;
-		this.selectedSprint = this.currentAndFutureSprints.find((sprint) => sprint.eid == selectedValue)!;
+		this.selectedSprint = this.allSprints.find((sprint) => sprint.eid == selectedValue)!;
+		this.loadMetrics();
 	}
 
-	loadLeadTimeMetrics() {
-		this.sprintApiService.getSprint(this.projectEid, this.currentSprint.eid).subscribe({
+	loadMetrics() {
+		this.sprintApiService.getSprint(this.projectEid, this.selectedSprint.eid).subscribe({
 			next: (sprintResponse) => {
-				this.detailedSprint = sprintResponse;
 				this.tasksFromSelectedSprint = sprintResponse.tasks;
-				this.calculateLeadTimeAverage(this.tasksFromSelectedSprint);
+				this.calculateLeadTime(this.tasksFromSelectedSprint);
+				this.calculateSprintVelocity(this.tasksFromSelectedSprint);
+				this.calculateAverageSprintsLeadTime();
+				this.calculateAverageSprintsVelocity();
 			},
 		});
 	}
 
-	calculateLeadTimeAverage(tasks: TaskSelfComposite[]) {
+	calculateLeadTime(tasks: TaskSelfComposite[]) {
+		if (this.selectedSprint.eid === this.currentSprint.eid) {
+			this.sprintLeadTime = "-";
+			return;
+		}
 		const leadTimes = tasks.map((task) => {
 			const createdAt = task.createdAt ? new Date(task.createdAt).getTime() : 0;
 			const completedAt = task.completedAt ? new Date(task.completedAt).getTime() : 0;
@@ -80,14 +94,124 @@ export class MainPageComponent {
 		const validLeadTimes = leadTimes.filter((leadTime) => leadTime > 0);
 
 		if (validLeadTimes.length === 0) {
-			this.leadTimeDaysAverage = 0;
+			this.sprintLeadTime = "-";
 			return;
 		}
 
 		const totalLeadTime = validLeadTimes.reduce((acc, leadTime) => acc + leadTime, 0);
 		const leadTimeAverage = totalLeadTime / validLeadTimes.length;
 
-		this.leadTimeDaysAverage = leadTimeAverage / (1000 * 60 * 60 * 24);
-		console.log(this.leadTimeDaysAverage);
+		const leadTimeDays = leadTimeAverage / (1000 * 60 * 60 * 24);
+
+		this.sprintLeadTime = Number.isInteger(leadTimeDays)
+			? leadTimeDays.toString()
+			: (Math.floor(leadTimeDays * 10) / 10).toFixed(1);
+	}
+
+	calculateAverageSprintsLeadTime() {
+		let totalLeadTime = 0;
+		let sprintCount = this.pastSprints.length;
+
+		if (sprintCount === 0) {
+			this.averageSprintsLeadTime = "-";
+			return;
+		}
+
+		let completedRequests = 0;
+
+		this.pastSprints.forEach((sprint) => {
+			this.sprintApiService.getSprint(this.projectEid, sprint.eid).subscribe((sprintResponse) => {
+				const leadTimes = sprintResponse.tasks.map((task) => {
+					const createdAt = task.createdAt ? new Date(task.createdAt).getTime() : 0;
+					const completedAt = task.completedAt ? new Date(task.completedAt).getTime() : 0;
+					return completedAt - createdAt;
+				});
+
+				const validLeadTimes = leadTimes.filter((leadTime) => leadTime > 0);
+
+				if (validLeadTimes.length > 0) {
+					const totalSprintLeadTime = validLeadTimes.reduce((acc, leadTime) => acc + leadTime, 0);
+					const sprintLeadTimeAverage = totalSprintLeadTime / validLeadTimes.length;
+					totalLeadTime += sprintLeadTimeAverage;
+				}
+
+				completedRequests++;
+
+				if (completedRequests === sprintCount) {
+					const averageLeadTime = totalLeadTime / sprintCount;
+					const leadTimeDays = averageLeadTime / (1000 * 60 * 60 * 24);
+					this.averageSprintsLeadTime = Number.isInteger(leadTimeDays)
+						? leadTimeDays.toString()
+						: (Math.floor(leadTimeDays * 10) / 10).toFixed(1);
+					this.setLeadTimeMetricImageUrl();
+				}
+			});
+		});
+	}
+
+	setLeadTimeMetricImageUrl() {
+		if (this.sprintLeadTime === "-") {
+			this.leadTimeMetricImageUrl = "";
+		} else {
+			if (parseFloat(this.sprintLeadTime) < parseFloat(this.averageSprintsLeadTime)) {
+				this.leadTimeMetricImageUrl = "../../../../../assets/metrics/metric-down-green.svg";
+			} else if (parseFloat(this.sprintLeadTime) > parseFloat(this.averageSprintsLeadTime)) {
+				this.leadTimeMetricImageUrl = "../../../../../assets/metrics/metric-up-red.svg";
+			} else {
+				this.leadTimeMetricImageUrl = "../../../../../assets/unassigned-user.svg";
+			}
+		}
+	}
+
+	calculateSprintVelocity(tasks: TaskSelfComposite[]) {
+		if (this.selectedSprint.eid === this.currentSprint.eid) {
+			this.sprintVelocity = "-";
+			return;
+		}
+		const completedTasks = tasks.filter((task) => task.status === TaskStatus.DONE);
+		this.sprintVelocity = completedTasks.reduce((total, task) => total + (task.complexity ?? 0), 0).toString();
+	}
+
+	calculateAverageSprintsVelocity() {
+		let totalVelocity = 0;
+		let sprintCount = this.pastSprints.length;
+
+		if (sprintCount === 0) {
+			this.averageSprintsVelocity = "-";
+			return;
+		}
+
+		let completedRequests = 0;
+
+		this.pastSprints.forEach((sprint) => {
+			this.sprintApiService.getSprint(this.projectEid, sprint.eid).subscribe((sprintResponse) => {
+				const completedTasks = sprintResponse.tasks.filter((task) => task.status === TaskStatus.DONE);
+				const sprintVelocity = completedTasks.reduce((total, task) => total + (task.complexity ?? 0), 0);
+				totalVelocity += sprintVelocity;
+				completedRequests++;
+
+				if (completedRequests === sprintCount) {
+					const averageVelocity = totalVelocity / sprintCount;
+					this.averageSprintsVelocity = Number.isInteger(averageVelocity)
+						? averageVelocity.toString()
+						: (Math.floor(averageVelocity * 10) / 10).toFixed(1);
+					this.setVelocityMetricImageUrl();
+				}
+			});
+		});
+	}
+
+	setVelocityMetricImageUrl() {
+		if (this.sprintVelocity === "-") {
+			this.velocityMetricImageUrl = "";
+		} else {
+			if (parseFloat(this.sprintVelocity) > parseFloat(this.averageSprintsVelocity)) {
+				this.velocityMetricImageUrl = "../../../../../assets/metrics/metric-up-green.svg";
+			} else if (parseFloat(this.sprintVelocity) < parseFloat(this.averageSprintsVelocity)) {
+				this.velocityMetricImageUrl = "../../../../../assets/metrics/metric-down-red.svg";
+			} else {
+				this.velocityMetricImageUrl = "../../../../../assets/unassigned-user.svg";
+			}
+		}
 	}
 }
