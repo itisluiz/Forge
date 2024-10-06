ce4590ac8e6e224735ecc2b88df7dab63f83c91c
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2024-10-06 17:33:17 -0300

    Created User Role method in userApiService and used in several pages

--- frontend/src/app/core/services/user-api.service.ts
+++ frontend/src/app/core/services/user-api.service.ts
@@ -1,11 +1,13 @@
 import { ApiService } from "./api.service";
 import { Injectable } from "@angular/core";
-import { Observable, tap } from "rxjs";
+import { map, Observable, switchMap, tap } from "rxjs";
 import { TokenService } from "./token.service";
 import { UserSelfResponse } from "forge-shared/dto/response/userselfresponse.dto";
 import { UserSigninRequest } from "forge-shared/dto/request/usersigninrequest.dto";
 import { UserSigninResponse } from "forge-shared/dto/response/usersigninresponse.dto";
 import { UserSignupRequest } from "forge-shared/dto/request/usersignuprequest.dto";
+import { ProjectApiService } from "./project-api.service";
+import { ProjectRole } from "forge-shared/enum/projectrole.enum";
 
 @Injectable({
 	providedIn: "root",
@@ -14,6 +16,7 @@ export class UserApiService {
 	constructor(
 		private apiService: ApiService,
 		private tokenService: TokenService,
+		private projectApiService: ProjectApiService,
 	) {}
 
 	private setTokenFromSigninResponse(userSigninResponse: UserSigninResponse) {
@@ -42,5 +45,36 @@ export class UserApiService {
 
 	public self(): Observable<UserSelfResponse> {
 		return this.apiService.call<UserSelfResponse>("GET", "user/self");
+	}
+
+	public getUserRoleForProject(projectEid: string): Observable<String | undefined> {
+		return this.self().pipe(
+			switchMap((user) => {
+				return this.projectApiService.getEspecificProject(projectEid).pipe(
+					map((project) => {
+						let memberRole = project.members.find((member) => member.eid === user.eid)?.role;
+						let stringRole = "";
+						switch (memberRole) {
+							case ProjectRole.PRODUCT_OWNER:
+								stringRole = "Product Owner";
+								break;
+							case ProjectRole.SCRUM_MASTER:
+								stringRole = "Scrum Master";
+								break;
+							case ProjectRole.DEVELOPER:
+								stringRole = "Developer";
+								break;
+							case ProjectRole.TESTER:
+								stringRole = "Tester";
+								break;
+							default:
+								stringRole = "No role";
+								break;
+						}
+						return stringRole;
+					}),
+				);
+			}),
+		);
 	}
 }
